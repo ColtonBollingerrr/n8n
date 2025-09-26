24ed9e99e62ab8e71582f29a6edf04f6e1ca4288
ColtonBollingerrr <127439148+ColtonBollingerrr@users.noreply.github.com>
2025-09-26 18:08:10 +0300

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
-# Map Cloud Run's $PORT to n8n's port if present
+set -e
+
+# Map Cloud Run's $PORT to n8n
 if [ -n "$PORT" ]; then
   export N8N_PORT="$PORT"
 fi
 
-# If you deploy behind HTTPS, set protocol accordingly (Cloud Run is HTTPS)
+# Cloud Run is HTTPS by default
 export N8N_PROTOCOL=${N8N_PROTOCOL:-https}
-# Bind host
 export N8N_HOST=${N8N_HOST:-0.0.0.0}
 
 # Start n8n
